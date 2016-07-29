658fc9349d64a7095ca8572615b51872a152d775
Palotec <joaopaulo164@hotmail.com>
2016-07-29 12:39:51 -0300

    Directive part 3 - Directives Controllers - Accordions

--- AngularJS_12_Criando_Diretivas_Parte_3/js/directives/uiAccordionDirective.js
+++ AngularJS_12_Criando_Diretivas_Parte_3/js/directives/uiAccordionDirective.js
@@ -7,8 +7,18 @@ angular.module("listaTelefonica").directive("uiAccordions", function () {
     //cria controller para compartilhar entre a directivas
     return {
         controller: function ($scope, $element, $attrs) {
-            this.helloWorld = function () {
-                console.log("Hello World")
+            var accordions = [];
+
+            // Registra o accordion
+            this.registerAccordion = function (accordion) {
+                accordions.push(accordion)
+            };
+
+            this.closeAll = function () {
+                accordions.forEach(function (accordion) {
+                    accordion.isOpened = false;
+
+                })
             }
         }
     };
@@ -25,9 +35,12 @@ angular.module("listaTelefonica").directive("uiAccordion", function () {
         },
         require: "^uiAccordions", //Acessa a API do pai uiAccordions, igual "ngModel" do uiDate só que naquele caso era local
         link: function (scope, element, attrs, ctrl) {
-            ctrl.helloWorld();
+
+            // Registrando accordion
+            ctrl.registerAccordion(scope);
             scope.open = function () {
-                scope.isOpened = !scope.isOpened;  //função open() do scope interno
+                ctrl.closeAll();
+                scope.isOpened = true;  //função open() do scope interno
             }
         }
     };
